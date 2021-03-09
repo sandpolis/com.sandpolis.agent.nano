6b8e0825590a7d4da330116d7b8b3025bdeaec2d
Tyler Cook <tcc@sandpolis.com>
2021-03-08 20:32:32 -0600

    Remove boot agent module (it's now rust)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,63 @@
 //                                                                            //
 //============================================================================//
 
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+import org.gradle.language.cpp.tasks.CppCompile
+import org.gradle.nativeplatform.tasks.LinkExecutable
+
 plugins {
-	id("java-library")
 	id("sandpolis-module")
-	id("sandpolis-java")
 	id("sandpolis-soi")
-	id("sandpolis-publish")
+	id("cpp-application")
+}
+
+application {
+	privateHeaders {
+		from(project.file("src/main/cpp"))
+		from(project(":module:com.sandpolis.core.foundation").file("gen/main/cpp"))
+		from(project(":module:com.sandpolis.core.instance").file("gen/main/cpp"))
+		from(project(":module:com.sandpolis.core.net").file("gen/main/cpp"))
+	}
+	source {
+		from(project.file("src/main/cpp"))
+		from(project(":module:com.sandpolis.core.foundation").file("gen/main/cpp"))
+		from(project(":module:com.sandpolis.core.instance").file("gen/main/cpp"))
+		from(project(":module:com.sandpolis.core.net").file("gen/main/cpp"))
+	}
+}
+
+tasks.withType<LinkExecutable> {
+	lib(file("/usr/lib/x86_64-linux-gnu/libprotobuf-lite.a"))
+	linkerArgs.set(listOf("-ldl", "-ltinfo", "-lpthread"))
+}
+
+tasks.withType<CppCompile> {
+	dependsOn(":module:com.sandpolis.core.foundation:generateProto")
+	dependsOn(":module:com.sandpolis.core.instance:generateProto")
+	dependsOn(":module:com.sandpolis.core.net:generateProto")
+	compilerArgs.set(listOf("-pthread", "-std=c++2a"))
+}
+
+// Inject resources into the executable
+val injectResources by tasks.creating(DefaultTask::class) {
+	dependsOn(":com.sandpolis.agent.micro:assembleDebug")
+	dependsOn(":com.sandpolis.agent.micro:writeSoi")
+
+	doLast {
+		val executable = project.file("build/exe/main/debug/com.sandpolis.agent.micro")
+
+		// Add resource section header (all zeros)
+		executable.appendBytes(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(0).array())
+
+		project.file("src/main/resources").walk().filter { it.isFile() }.forEach {
+
+			// Write the file followed by the file size, path, and path size
+			executable.appendBytes(it.readBytes())
+			executable.appendBytes(ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putLong(it.length()).array())
+			executable.appendText(it.getName())
+			executable.appendBytes(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(it.getName().length).array())
+		}
+	}
 }
